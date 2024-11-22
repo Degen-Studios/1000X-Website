7c7825f8faa5d9b4fe35cbdfa9da4026556d8906
flyingmongoose <flyingmongoose@users.noreply.github.com>
2024-11-22 21:04:29 +0000

    deploy: 17a68da5e7ef9a1f6b67d1fcbc53053cda766e74

---
+++ assets/js/contract.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const contractAddress = document.querySelector('.contract-address');
+
+    if (contractAddress) {
+        const updateTooltipPosition = (e, tooltip) => {
+            const tooltipWidth = tooltip.offsetWidth;
+            const mouseX = e.clientX;
+            const mouseY = e.clientY;
+            tooltip.style.left = `${mouseX - (tooltipWidth / 2)}px`;
+            tooltip.style.top = `${mouseY - 30}px`;
+        };
+
+        contractAddress.addEventListener('mousemove', (e) => {
+            const tooltip = document.querySelector('.contract-tooltip');
+            if (tooltip) {
+                updateTooltipPosition(e, tooltip);
+            }
+        });
+
+        contractAddress.addEventListener('mouseenter', () => {
+            const tooltip = document.createElement('div');
+            tooltip.className = 'contract-tooltip';
+            tooltip.textContent = 'Click to Copy';
+            document.body.appendChild(tooltip);
+        });
+
+        contractAddress.addEventListener('mouseleave', () => {
+            const tooltip = document.querySelector('.contract-tooltip');
+            if (tooltip) {
+                tooltip.remove();
+            }
+        });
+
+        contractAddress.addEventListener('click', (e) => {
+            const tooltip = document.querySelector('.contract-tooltip');
+            navigator.clipboard.writeText(contractAddress.textContent.trim());
+            if (tooltip) {
+                tooltip.textContent = 'Copied';
+                updateTooltipPosition(e, tooltip);
+
+                setTimeout(() => {
+                    if (tooltip && document.contains(tooltip)) {
+                        tooltip.textContent = 'Click to Copy';
+                        updateTooltipPosition(e, tooltip);
+                    }
+                }, 2000);
+            }
+        });
+    }
+});
